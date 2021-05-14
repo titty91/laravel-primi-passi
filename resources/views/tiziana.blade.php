1d9aca7149147e72c1ec5e421d1e3cfefd55d56b
titty91 <tznbaraldi@gmail.com>
2021-05-14 16:06:29 +0200

    primi passi in laravel

---
+++ resources/views/tiziana.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+  </head>
+  <body>
+    <h1>ciao mondo</h1>
+  </body>
+</html>
